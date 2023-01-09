c70c7dc51d643f93ada2817bd531d2c9e626aed2
yvalois <yeison.valopez@gmail.com>
2023-01-09 18:02:59 -0500

    a

--- src/components/nft/select-wallet.tsx
+++ src/components/nft/select-wallet.tsx
@@ -86,15 +86,15 @@ export default function SelectWallet({ ...props }) {
         </div>
 
         <div
-          className="mt-12 flex h-14 w-full cursor-pointer items-center justify-center rounded-lg bg-gradient-to-l from-[#ffdc24] to-[#ff5c00] px-4 text-base text-white transition-all hover:-translate-y-0.5"
+          className="mt-12 flex h-14 w-full cursor-pointer items-center justify-between rounded-lg bg-gradient-to-l from-[#ffdc24] to-[#ff5c00] px-4 text-base text-white transition-all hover:-translate-y-0.5"
           onClick={abrir}
         >
-          {/*<span>Conectar Cartera</span>
-           <span className="h-auto w-9">
+          <span>Conectar con metamask</span>
+          <span className="h-auto w-9">
             <Image src={metamaskLogo} alt="metamask" />
-          </span> */}
+          </span>
         </div>
-        <ConnectKitButton />
+        {/* <ConnectKitButton />  */}
 
         {/* <MobileView>
             <h1>XD</h1>
