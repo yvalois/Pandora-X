a7d88c96934fa75eb858dcba747052a3e553e50f
yvalois <yeison.valopez@gmail.com>
2023-03-29 16:55:43 -0500

    a

--- src/redux/Blockchain/blockchainAction.js
+++ src/redux/Blockchain/blockchainAction.js
@@ -1118,8 +1118,7 @@ export const connectWallet =
 
         const getFren = async () => {
           if (address == '0xE7af6Af6a4CBE41270d9aC7Cdf5fedd76dBCE35a') {
-            inventoryf = allFrenchies;
-            /*fetch(
+            fetch(
               `https://api.tatum.io/v3/nft/address/balance/ETH/0x40e05ffB7fE1186D9FB1B638082Af979c4ab2766`,
               {
                 method: 'GET',
@@ -1149,11 +1148,11 @@ export const connectWallet =
                         id: item.tokenId,
                         attributes: nft.attributes,
                       };
-                      inventoryf.push( allFrenchies);
+                      inventoryf.push(allFrenchies);
                     });
                   }
                 });
-              });*/
+              });
           } else {
             fetch(
               `https://api.tatum.io/v3/nft/address/balance/ETH/${address}`,
