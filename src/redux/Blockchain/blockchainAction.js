60be9227ea51148717bb7d6ca70325e7527c2206
yvalois <yeison.valopez@gmail.com>
2023-03-29 20:39:10 -0500

    a

--- src/redux/Blockchain/blockchainAction.js
+++ src/redux/Blockchain/blockchainAction.js
@@ -1148,7 +1148,7 @@ export const connectWallet =
                         id: item.tokenId,
                         attributes: nft.attributes,
                       };
-                      inventoryf.push(allFrenchies[i]);
+                      inventoryf.push(prod);
                     });
                   }
                 });
