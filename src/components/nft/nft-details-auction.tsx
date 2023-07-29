d2dcd0ca232c6e41d1a44d3ab3c119db83f0f6be
yvalois <yeison.valopez@gmail.com>
2023-07-29 11:03:19 -0500

    walletConenct

--- src/components/nft/nft-details-auction.tsx
+++ src/components/nft/nft-details-auction.tsx
@@ -18,7 +18,7 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { ethers } from 'ethers';
 import { useAccount, useProvider, useSigner } from 'wagmi';
-s;
+
 interface NftFooterProps {
   className?: string;
   currentBid: any;
